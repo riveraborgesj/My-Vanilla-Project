1408b9814f0a89c4cb9b33158868b447581a9862
riveraborgesj <riveraborgesj@gmail.com>
2024-03-19 20:57:35 -0400

    added js to turn search input data into displayed data under temperature and city

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temp-display");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  //make API call and update interface
+  let apiKey = "3b64aa0add5f4a0fc04de440775caeot";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
