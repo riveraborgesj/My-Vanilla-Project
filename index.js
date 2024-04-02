dddf7a41e25a3261407490b720ed55414be3c00f
riveraborgesj <riveraborgesj@gmail.com>
2024-04-02 12:24:29 -0400

    used API to change icon image
    used API to change icon image and added animation

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function updateWeatherInfo(response) {
   let cityElement = document.querySelector("#city");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   timeElement.innerHTML = formatDate(date);
   windElement.innerHTML = Math.round(windSpeed);
