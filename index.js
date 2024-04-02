962ff990c8cb54f668867f58d814ce61560489f8
riveraborgesj <riveraborgesj@gmail.com>
2024-04-02 10:01:23 -0400

    format date
    created function that formats the date

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 function updateWeatherInfo(response) {
   let temperatureElement = document.querySelector("#temp-display");
+  let weatherDescription = document.querySelector("#description");
   let temperature = response.data.temperature.current;
+  let humidityDisplay = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
   let cityElement = document.querySelector("#city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  timeElement.innerHTML = formatDate(date);
+  windElement.innerHTML = Math.round(windSpeed);
+  humidityDisplay.innerHTML = Math.round(humidity);
+  weatherDescription.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
